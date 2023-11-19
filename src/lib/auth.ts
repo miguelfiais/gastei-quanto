b99af08826e0a19ea354b09e2430ffddbafc6ba8
miguelfiais <miguel.fiais10@gmail.com>
2023-11-19 16:40:22 -0300

    feat: add authentication with google and credentials

---
+++ src/lib/auth.ts
@@ -0,0 +1,50 @@
+import { AuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import prisma from './db'
+import { User } from '@prisma/client'
+import bcrypt from 'bcrypt'
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: 'crendentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'text',
+          placeholder: 'johndoe@gmail.com',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials): Promise<User | null> {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Dados de Login necessários')
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        })
+        if (!user || !user.hashedPassword) {
+          return null
+        }
+        const matchPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword,
+        )
+        if (!matchPassword) {
+          return null
+        }
+        return user
+      },
+    }),
+  ],
+  secret: process.env.SECRET_KEY,
+}
