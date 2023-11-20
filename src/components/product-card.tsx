18e9a1976698dbc0b878182e53787d0d2e79dc33
miguelfiais <miguel.fiais10@gmail.com>
2023-11-20 16:29:03 -0300

    feat: add link to product page

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -3,6 +3,7 @@
 import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 import { Product } from '@prisma/client'
 import { LoaderIcon, PenIcon, TrashIcon } from 'lucide-react'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
@@ -27,24 +28,26 @@ const ProductCard = ({ product }: ProductCardProps) => {
     setIsLoading(false)
   }
   return (
-    <Card>
-      <CardHeader className="flex-row items-center justify-between p-4">
-        <PenIcon className="h-4 w-4" />
-        <CardTitle className="flex flex-col text-center text-xl capitalize">
-          {product.name}
-          <span className="text-xs font-medium">
-            Unidades: {product.quantity}
-          </span>
-        </CardTitle>
-        <button onClick={deleteProduct} disabled={isLoading}>
-          {isLoading ? (
-            <LoaderIcon className="h-4 w-4 animate-spin text-destructive" />
-          ) : (
-            <TrashIcon className="h-4 w-4 text-destructive" />
-          )}
-        </button>
-      </CardHeader>
-    </Card>
+    <Link href={`/produtos/${product.id}`}>
+      <Card>
+        <CardHeader className="flex-row items-center justify-between p-4">
+          <PenIcon className="h-4 w-4" />
+          <CardTitle className="flex flex-col text-center text-xl capitalize">
+            {product.name}
+            <span className="text-xs font-medium">
+              Unidades: {product.quantity}
+            </span>
+          </CardTitle>
+          <button onClick={deleteProduct} disabled={isLoading}>
+            {isLoading ? (
+              <LoaderIcon className="h-4 w-4 animate-spin text-destructive" />
+            ) : (
+              <TrashIcon className="h-4 w-4 text-destructive" />
+            )}
+          </button>
+        </CardHeader>
+      </Card>
+    </Link>
   )
 }
 
