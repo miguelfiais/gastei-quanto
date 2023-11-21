6f79fdfb8d69e9fdc53ef63972466c2df9847ff0
miguelfiais <miguel.fiais10@gmail.com>
2023-11-20 22:23:56 -0300

    feat: add function to calculate ingredient cost

---
+++ src/helpers/ingredientCost.tsx
@@ -0,0 +1,11 @@
+import { Ingredient } from '@prisma/client'
+
+interface IngredientWithCost extends Ingredient {
+  cost: number
+}
+export const calculateCost = (ingredient: Ingredient): IngredientWithCost => {
+  const cost =
+    (Number(ingredient.price) * ingredient.quantityUsed) / ingredient.quantity
+  const ingredientWithCost = { ...ingredient, cost }
+  return ingredientWithCost
+}
