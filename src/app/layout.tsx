ee87c80b8c4f47a6f1778f96b9e925d10c7c04f9
miguelfiais <miguel.fiais10@gmail.com>
2023-11-25 16:54:13 -0300

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { cn } from '@/lib/utils'
 import { Toaster } from '@/components/ui/toaster'
 import Footer from '@/components/footer'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         {children}
         <Toaster />
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
