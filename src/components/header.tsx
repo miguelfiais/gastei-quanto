918b5391a9ba4fd732b6942e63e0b0ea6bbe1080
miguelfiais <miguel.fiais10@gmail.com>
2023-11-25 15:29:08 -0300

    feat: add user image to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,8 @@
 import { signOut } from 'next-auth/react'
 import { Button } from './ui/button'
 import Link from 'next/link'
+import Image from 'next/image'
+import { User2Icon } from 'lucide-react'
 
 interface HeaderProps {
   user: {
@@ -18,7 +20,22 @@ const Header = ({ user }: HeaderProps) => {
 
   return (
     <header className="container flex items-center justify-between py-4">
-      <Link href={'/produtos'} className="text-sm font-medium">
+      <Link
+        href={'/produtos'}
+        className="flex items-center gap-2 text-sm font-medium"
+      >
+        {user.image ? (
+          <Image
+            src={user.image}
+            alt={user.name ?? 'foto do usuario'}
+            width={32}
+            height={32}
+            className="rounded-full"
+          />
+        ) : (
+          <User2Icon className="h-8 w-8" />
+        )}
+
         {user.name}
       </Link>
       <Button variant={'link'} onClick={logout}>
