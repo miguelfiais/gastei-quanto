eb75d86d22530e67dfd6363fe9f4d13f544935c8
miguelfiais <miguel.fiais10@gmail.com>
2023-11-25 16:51:02 -0300

    feat: add ingredient delete function

--- src/components/ingredient-card.tsx
+++ src/components/ingredient-card.tsx
@@ -1,12 +1,29 @@
-import { PenIcon, TrashIcon } from 'lucide-react'
+'use client'
+import { LoaderIcon, PenIcon, TrashIcon } from 'lucide-react'
 import { Card, CardHeader } from './ui/card'
 import { IngredientWithCost } from '@/helpers/ingredientCost'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 interface IngredientCardProps {
   ingredient: IngredientWithCost
 }
 
 const IngredientCard = ({ ingredient }: IngredientCardProps) => {
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+  const deleteIngredient = async () => {
+    setIsLoading(true)
+    await fetch('/api/ingredient', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: ingredient.id }),
+    })
+    router.refresh()
+    setIsLoading(false)
+  }
   return (
     <Card key={ingredient.id}>
       <CardHeader className="flex-row items-center justify-between p-3">
@@ -20,8 +37,12 @@ const IngredientCard = ({ ingredient }: IngredientCardProps) => {
           </p>
         </div>
 
-        <button>
-          <TrashIcon className="h-4 w-4 text-destructive" />
+        <button onClick={deleteIngredient} disabled={isLoading}>
+          {isLoading ? (
+            <LoaderIcon className="h-4 w-4 animate-spin text-destructive" />
+          ) : (
+            <TrashIcon className="h-4 w-4 text-destructive" />
+          )}
         </button>
       </CardHeader>
     </Card>
